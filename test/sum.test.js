549e4c018de099fbfcf303abac91359642ec037f
ihtisham-123 <baberihtasham640@gmail.com>
2025-10-22 22:38:15 +0500

    my intital commit

---
+++ test/sum.test.js
@@ -0,0 +1,8 @@
+import {decsribe, it, expect} from 'vitest';
+import { sum } from '../src/sum.js';
+
+describe('sum', () => {
+    it('should return the sum of two numbers', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+});
